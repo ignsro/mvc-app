cb33df1e91604a63efe5b95bdbac9f14c333b29b
ignsp <ignsp@gmail.com>
2021-09-04 15:05:27 -0400

    update: container registers

--- backend/startup/container.js
+++ backend/startup/container.js
@@ -1,15 +1,47 @@
-import { createContainer, asClass, asFunction, asValue } from 'awilix'
+const { createContainer, asClass, asFunction, asValue } = require('awilix')
 
 //Config
-import config from '../config';
-import app from '.'
+const config = require('../config');
+const app = require('.');
 
+//Models
+const db = require('../models')
+
+//Repositories
+const { ClientRepository } = require('../repositories')
+
+//Services
+const { ClientService } = require('../services')
+
+//Controllers
+const { ClientController } = require('../controllers')
+
+//Routes
+const routes = require('../routes'); 
+const { ClientRoutes } = require('../routes/index.routes');
 
 const container = createContainer();
 
 container
     .register({
         app: asClass(app).singleton(),
-        config: asValue(config).singleton(),
-        
+        router: asFunction(routes).singleton(),
+        config: asValue(config)
     })
+    .register({
+        Client: asValue(db['Client'])
+    })
+    .register({
+        ClientRepository: asClass(ClientRepository).singleton()
+    })
+    .register({
+        ClientService: asClass(ClientService).singleton()
+    })
+    .register({
+        ClientController: asClass(ClientController.bind(ClientController)).singleton()
+    })
+    .register({
+        ClientRoutes: asFunction(ClientRoutes).singleton()
+    });
+
+module.exports = container;
--- backend/startup/index.js
+++ backend/startup/index.js
@@ -1,4 +1,18 @@
-const app = ''
+const express = require("express")
+class App {
+    constructor({ config, router}){
+        this.config = config;
+        this.express = express().use(router)
+    }
 
+    start(){
+        return new Promise(resolve => {
+            this.express.listen(this.config.PORT, () => {
+                console.log(`${this.config.APPLICATION_NAME} is running on port ${this.config.PORT}`)
+            })
+            resolve();
+        })
+    }
+}
 
-export default app;
+module.exports = App;
