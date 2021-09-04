8b9ff7c274d3da608a7aa008555dbd4e5adb9b0f
ignsp <ignsp@gmail.com>
2021-09-04 15:06:58 -0400

    add: index files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,25 @@
-const routes = {
+const express = require("express")
+const { Router } = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares")
+require('express-async-errors')
 
+module.exports  = function({ ClientRoutes }) {
+    const router = Router();
+    const apiRoutes = express.Router();
+
+    apiRoutes
+        .use(express.json())
+        .use(cors())
+        .use(helmet())
+
+    apiRoutes.use("/client", ClientRoutes)
+
+    router.use("/v1/api", apiRoutes)
+
+    router.use(NotFoundMiddleware);
+    //router.use(ErrorMiddleware);
+
+    return router
 }
-
-
-export default routes;
---
+++ backend/services/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    ClientService: require('./client.service')
+}
