bf8af355c8493187b2b4659819c480ef9ef2c5cc
ignsp <ignsp@gmail.com>
2021-09-04 20:30:36 -0400

    update: model migrates

--- backend/migrations/20210904213840-create-detail.js
+++ backend/migrations/20210904213840-create-detail.js
@@ -19,14 +19,6 @@ module.exports = {
       },
       subtotal: {
         type: Sequelize.FLOAT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
